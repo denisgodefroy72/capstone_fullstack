0735277ed06ea8107f7f9dbe0c0db963a8c201f5
dgodefroy <dgodefroy@7fois6.com>
2025-09-25 06:58:59 -0400

    add IBM code image

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     '*',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    '*'
+    'https://prodgodefroy-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
